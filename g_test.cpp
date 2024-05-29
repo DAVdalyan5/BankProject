8550a8c9668f56f6a185de8b1a87edcccbc4f897
DAVdalyan5 <136731306+DAVdalyan5@users.noreply.github.com>
2024-05-29 15:57:56 +0400

    Create g_test.cpp

---
+++ g_test.cpp
@@ -0,0 +1,21 @@
+#include "Bank.h"
+#include <gtest/gtest.h>
+
+TEST(BankTest, TestTransfer) {
+    Bank bank(2, 1000);
+    bank.modifyAllBalances(500); // Зачислим на оба счета по 500 единиц
+    bank.transfer(0, 1, 300);
+    ASSERT_EQ(bank.getAccountBalance(0), 200);
+    ASSERT_EQ(bank.getAccountBalance(1), 800);
+}
+
+TEST(BankTest, TestFreezeAccount) {
+    Bank bank(1, 500);
+    bank.setAccountFrozen(0, true);
+    ASSERT_THROW(bank.modifyAllBalances(100), std::runtime_error);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
